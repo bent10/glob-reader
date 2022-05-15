b020bdc57aa66fe70f73887aeea8228abc649ffb
bent10 <bent10@stilearning.com>
2022-05-15 19:36:14 +0700

    fix(release): git rm dist

--- dist/async.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="node" />
-import { File } from './File.js';
-import { Options } from './types.js';
-/**
- * Asynchronously expands glob patterns for iterative file and text processing.
- */
-export declare function readGlob(patterns: string | readonly string[], options?: BufferEncoding | Options): AsyncGenerator<File>;
